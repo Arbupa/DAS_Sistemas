ccd2a234471681222ecd4bed2a358dbc77b05c4f
Arnoldo <juan.arnoldo.munoz@hotmail.com>
2020-10-11 23:14:36 -0500

    Practica-1 sin archivos de más

--- Ago-Dic-2020/chavez-muñoz-juan-arnoldo/Practica-1/Practica-1.py
+++ Ago-Dic-2020/chavez-muñoz-juan-arnoldo/Practica-1/Practica-1.py
@@ -8,6 +8,6 @@ class Matrix:
     def column(self, index):
         return [self.matrix_string[i][index-1] for i in range(len(self.matrix_string))]
         
-matriz = Matrix("9 8 7\n 5 3 2\n6 6 7") #Creo la instancia de la clases
+matriz = Matrix("9 8 7\n 5 3 2\n6 6 7")#Creo la instancia de la clases
 print(matriz.row(1))
 print(matriz.column(2))
